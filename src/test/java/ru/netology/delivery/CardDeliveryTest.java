14e98516cbfd9d3a6ca7fb1e38c1bf010f11c576
Neto test <veronikakaravanova60@gmail.com>
2025-12-07 21:59:59 +0500

    Simple project structure for CI

--- src/test/java/ru/netology/delivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/CardDeliveryTest.java
@@ -1,11 +1,4 @@
-package ru.netology.delivery;
-
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+﻿package ru.netology.delivery;
 public class CardDeliveryTest {
-    @Test
-    void ciTest() {
-        assertTrue(true);
-    }
+    // Тесты будут здесь
 }
